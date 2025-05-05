9e94916c3d903ecbdc811f1acd0615602f006913
c-o-b-a-x <cobyhughes2008@gmail.com>
2025-05-05 09:26:12 -0400

    finished product upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const btn = document.getElementById("toggle-btn");
+
+  sidebar.classList.toggle("collapsed");
+}
